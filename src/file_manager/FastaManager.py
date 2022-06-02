d2786af8bb5275a68f02fe531f239db45aaf5417
Abner Silveira de Freitas <defreitasabner@gmail.com>
2022-06-02 00:35:31 -0300

    read_file() method in FastaManager was implemented. Some little fixes in FileManager import and swap ABCMeta to ABC to define AbstractClass

--- src/file_manager/FastaManager.py
+++ src/file_manager/FastaManager.py
@@ -1,12 +1,31 @@
-from FileManager import FileManager
+from typing import List, Dict
+
+from .FileManager import FileManager
 
 
 class FastaManager(FileManager):
 
     @staticmethod
-    def read_file(path: str):
-        with open(path, mode='r', encoding='utf-8'):
-            ...
+    def read_file(path: str) -> List[Dict[str, str]]:
+        taxa: List[str] = []
+        sequences: List[str] = []
+        with open(path, mode='r', encoding='utf-8') as fasta_file:
+            for line in fasta_file:
+                if '>' in line:
+                    taxon: str = line
+                    taxon_treated: str = taxon.replace('>', '').strip()
+                    taxa.append(taxon_treated)
+                else:
+                    sequence: str = line.strip()
+                    sequences.append(sequence)
+        if len(taxa) == len(sequences):
+            dataset: List[Dict[str:str]] = []
+            for i in range(len(taxa)):
+                data = {'taxon': taxa[i], 'sequence': sequences[i]}
+                dataset.append(data)
+            return dataset
+        else:
+            raise Exception('The number of taxa is different of the number of sequences')
 
     @staticmethod
     def write_file(path: str):
--- src/file_manager/FileManager.py
+++ src/file_manager/FileManager.py
@@ -1,7 +1,7 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 
 
-class FileManager(meta=ABCMeta):
+class FileManager(ABC):
 
     @staticmethod
     @abstractmethod
