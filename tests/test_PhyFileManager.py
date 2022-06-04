25de18a9c05b65ec3b7154c34bbd12c6126a8374
Abner Silveira de Freitas <defreitasabner@gmail.com>
2022-06-04 00:32:22 -0300

    Starting to write tests to PhyFileManager. Example data to .phy refactored.

---
+++ tests/test_PhyFileManager.py
@@ -0,0 +1,19 @@
+import pytest
+
+from src.file_manager.PhyFileManager import PhyFileManager
+
+class TestPhyFileManager:
+
+    @pytest.fixture
+    def example_phy_path(self):
+        path = 'example.phy'
+        return path
+
+    @pytest.fixture
+    def correct_output_to_example_phy(self):
+        correct_output = [{'taxon': 'H_sapiens', 'sequence': 'ACCGGTTGGCCGTTCAGGGTACAGGTTGGCCGTTCAGGGTAA'}, {'taxon': 'Chimp', 'sequence': 'AAACCCTTGCCGTTACGCTTAAACCGAGGCCGGGACACTCAT'}, {'taxon': 'Gorilla', 'sequence': 'AAACCCTTGCCGGTACGCTTAAACCATTGCCGGTACGCTTAA'}]
+        return correct_output
+
+    def test_read_file_method_with_correct_input(self, example_phy_path, correct_output_to_example_phy):
+        output_data = PhyFileManager.read_file(example_phy_path)
+        assert output_data == correct_output_to_example_phy
