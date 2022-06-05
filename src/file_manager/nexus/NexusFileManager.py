6d5820135d5adf71a4eb689e3897b4a2ce90a0f6
Abner Silveira de Freitas <defreitasabner@gmail.com>
2022-06-05 00:01:50 -0300

    Method extract_info_to_header implemented. NexusFileManager working to one sequence (concatenated or not).

--- src/file_manager/nexus/NexusFileManager.py
+++ src/file_manager/nexus/NexusFileManager.py
@@ -20,12 +20,20 @@ class NexusFileManager(FileManager):
 
     @staticmethod
     def write_file(output_path: str, output_data: List[Dict[str, str]]):
-        number_taxa, number_characters, symbols = NexusFileManager.extract_info_to_header(output_data)
+        number_taxa, number_characters, symbols, missing, gap = NexusFileManager.extract_info_to_header(output_data)
         with open(output_path, mode='w', encoding='utf-8') as nexus_file:
             first_line = '#NEXUS\n\n'
             begin_data = 'BEGIN DATA;\n'
-            header_first_line = f'  DIMENSIONS NTAX={number_taxa} NCHAR={number_characters}\n'
-            header_second_line = f' FORMAT SYMBOLS="{symbols}" MISSING=N GAP=- interleave=no;\n'
+            data_first_line = f'    DIMENSIONS NTAX={number_taxa} NCHAR={number_characters};\n'
+            data_second_line = f'    FORMAT SYMBOLS="{symbols}" MISSING={missing} GAP={gap} interleave=no;\n\n'
+            matrix = 'MATRIX\n\n'
+            header = [first_line, begin_data, data_first_line, data_second_line, matrix]
+            for line in header:
+                nexus_file.write(line)
+            for data in output_data:
+                line_string = f'{data["taxon"]} {data["sequence"]}\n'
+                nexus_file.write(line_string)
+            nexus_file.write('\n;\nEND;\n')
 
     @staticmethod
     def extract_info_to_header(output_data: List[Dict[str, str]]):
@@ -37,9 +45,10 @@ class NexusFileManager(FileManager):
             extracted_symbols = list(set(list(all_sequences)))
             symbols = ''
             for item in extracted_symbols:
-                symbol = f'{item} '
-                if extracted_symbols.index(item) == len(extracted_symbols) - 1:
-                    symbol = symbol.replace(' ', '')
-                symbols += symbol
-            header_info = (number_taxa, number_characters, symbols)
+                if (item != 'N') and (item != '-'):
+                    symbol = f'{item} '
+                    if extracted_symbols.index(item) == len(extracted_symbols) - 1:
+                        symbol = symbol.replace(' ', '')
+                    symbols += symbol
+            header_info = (number_taxa, number_characters, symbols, 'N', '-')
             return header_info
