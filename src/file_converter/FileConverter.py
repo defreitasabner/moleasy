73883ea8a3d19d04e7c25c72d510b4d167e6a7d6
Abner Silveira de Freitas <defreitasabner@gmail.com>
2022-06-05 19:01:24 -0300

    FileConverter implemented

--- src/file_converter/FileConverter.py
+++ src/file_converter/FileConverter.py
@@ -8,7 +8,7 @@ class FileConverter:
 
     @staticmethod
     def convert(input_path: str, output_file_extension: str):
-        
+
         if input_path.endswith(('.fasta', '.fas')):
             if output_file_extension == 'phyllip':
                 FileConverter.fas_to_phy(input_path)
@@ -31,26 +31,31 @@ class FileConverter:
             raise Exception('Unsupported file extension, try again with fasta, phyllip or nexus file.')
 
     @staticmethod
-    def fas_to_phy(input_path: str, output_path: str = 'output.phy') -> None:
+    def fas_to_phy(input_path: str) -> None:
         fasta_data: List[Dict[str, str]] = FastaFileManager.read_file(input_path)
-        PhyFileManager.write_file(output_path, fasta_data)
+        PhyFileManager.write_file(fasta_data)
     
     @staticmethod
-    def fas_to_nex():
-        ...
+    def fas_to_nex(input_path: str) -> None:
+        fasta_data: List[Dict[str, str]] = FastaFileManager.read_file(input_path)
+        NexusFileManager.write_file(fasta_data)
 
     @staticmethod
-    def phy_to_fas():
-        ...
+    def phy_to_fas(input_path: str) -> None:
+        phyllip_data: List[Dict[str, str]] = PhyFileManager.read_file(input_path)
+        FastaFileManager.write_file(phyllip_data)
     
     @staticmethod
-    def phy_to_nex():
-        ...
+    def phy_to_nex(input_path: str):
+        phyllip_data: List[Dict[str, str]] = PhyFileManager.read_file(input_path)
+        NexusFileManager.write_file(phyllip_data)
 
     @staticmethod
-    def nex_to_fas():
-        ...
+    def nex_to_fas(input_path: str):
+        nexus_data: List[Dict[str, str]] = NexusFileManager.read_file(input_path)
+        FastaFileManager.write_file(nexus_data)
     
     @staticmethod
-    def nex_to_phy():
-        ...
+    def nex_to_phy(input_path):
+        nexus_data: List[Dict[str, str]] = NexusFileManager.read_file(input_path)
+        PhyFileManager.write_file(nexus_data)
