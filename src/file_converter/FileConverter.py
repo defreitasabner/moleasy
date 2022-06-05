f2f18f4308c111a1e4a090da0c4aab84ea724947
Abner Silveira de Freitas <defreitasabner@gmail.com>
2022-06-05 17:07:34 -0300

    Starting to implement FileConverter module, a factory to convert files using FileManager's Classes static methods.

---
+++ src/file_converter/FileConverter.py
@@ -0,0 +1,29 @@
+class FileConverter:
+
+    @staticmethod
+    def check_file_extension(input_path: str):
+        ...
+
+    @staticmethod
+    def fas_to_phy():
+        ...
+    
+    @staticmethod
+    def fas_to_nex():
+        ...
+
+    @staticmethod
+    def phy_to_fas():
+        ...
+    
+    @staticmethod
+    def phy_to_nex():
+        ...
+
+    @staticmethod
+    def nex_to_fas():
+        ...
+    
+    @staticmethod
+    def nex_to_phy():
+        ...
