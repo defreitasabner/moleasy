1a4af09764f98952d4d936a182fd57a01c23632b
Abner Silveira de Freitas <defreitasabner@gmail.com>
2022-06-04 22:56:32 -0300

    Implemented function to extract data to header of nexus file. Add a complex nexus example data.

--- src/file_manager/nexus/NexusFileManager.py
+++ src/file_manager/nexus/NexusFileManager.py
@@ -4,11 +4,42 @@ from ..FileManager import FileManager
 
 
 class NexusFileManager(FileManager):
-    
+
     @staticmethod
-    def read_file(input_path: str):
-        ...
+    def read_file(input_path: str): # Need to implement yet, some difficults with complex nexus file
+        regex_patterns = {
+            'interleaves': '',
+            'taxa': '[A-Z][a-z]*[_][a-z]*',
+            'sequences': '[ATCGN]+[-]*' # also catchs isolated words
+        }
+        taxa = []
+        sequences = []
+        with open(input_path, mode='r', encoding='utf-8') as nexus_file:
+            for line in nexus_file:
+                print(line)
 
     @staticmethod
     def write_file(output_path: str, output_data: List[Dict[str, str]]):
-        ...
+        number_taxa, number_characters, symbols = NexusFileManager.extract_info_to_header(output_data)
+        with open(output_path, mode='w', encoding='utf-8') as nexus_file:
+            first_line = '#NEXUS\n\n'
+            begin_data = 'BEGIN DATA;\n'
+            header_first_line = f'  DIMENSIONS NTAX={number_taxa} NCHAR={number_characters}\n'
+            header_second_line = f' FORMAT SYMBOLS="{symbols}" MISSING=N GAP=- interleave=no;\n'
+
+    @staticmethod
+    def extract_info_to_header(output_data: List[Dict[str, str]]):
+            number_taxa = len(output_data)
+            number_characters = len(output_data[0]['sequence'])
+            all_sequences = ''
+            for data in output_data:
+                all_sequences += data['sequence']
+            extracted_symbols = list(set(list(all_sequences)))
+            symbols = ''
+            for item in extracted_symbols:
+                symbol = f'{item} '
+                if extracted_symbols.index(item) == len(extracted_symbols) - 1:
+                    symbol = symbol.replace(' ', '')
+                symbols += symbol
+            header_info = (number_taxa, number_characters, symbols)
+            return header_info
