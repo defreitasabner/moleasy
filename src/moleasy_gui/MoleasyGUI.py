a7d7a51e20125d6c74bc89f5495d4e847611bbbd
Abner Silveira de Freitas <defreitasabner@gmail.com>
2022-06-08 20:07:11 -0300

    Base of GUI implemented, however withour any utility aplied and with bugs, as eternal loop

--- src/moleasy_gui/MoleasyGUI.py
+++ src/moleasy_gui/MoleasyGUI.py
@@ -3,20 +3,30 @@ import PySimpleGUI as sg
 
 class MoleasyGUI:
     def __init__(self):
+        sg.theme('DarkGrey13')
         # Layout
         layout = [
-            [sg.Button('Button')]
+            [sg.Output(size=(50, 10), key='output_log')],
+            [sg.Text('Input:'), sg.In(size=(33)), sg.FileBrowse()],
+            [sg.Text('Output:'), sg.In(size=(32)), sg.FileBrowse()],
+            [sg.Button('Convert files', key='convert_file'), sg.Button('Concat files', key='concat_file')]
         ]
         
         # Window
-        window = sg.Window('Moleasy').layout(layout)
+        self.window = sg.Window('Moleasy').layout(layout)
         
         # Data extracted
-        self.button, self.value = window.Read()
+        self.event, self.values = self.window.Read()
 
     def start(self):
-        print('Test')
-
+        while True:
+            if self.event == sg.WIN_CLOSED or self.event == 'Exit':
+                break
+            if self.event == 'convert_file':
+                print('Converting...')
+            elif self.event == 'concat_file':
+                print('Concat...')
+        self.window.close()
 
 window = MoleasyGUI()
 window.start()
