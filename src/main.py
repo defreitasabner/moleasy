a42816d61df0192ecc366469bf014b4c205d08d0
Abner Silveira de Freitas <defreitasabner@gmail.com>
2022-06-21 06:54:58 -0300

    Implemented if, elif, and else, to check if the user want to use CLI or GUI

--- src/main.py
+++ src/main.py
@@ -2,12 +2,20 @@ import sys
 from typing import List
 
 from moleasy_cli.MoleasyCLI import MoleasyCLI
+from moleasy_gui.MoleasyGUI import MoleasyGUI
 
 def main() -> None:
 
     args: List[str] = sys.argv
 
-    MoleasyCLI(args)
+    if len(args) > 2:
+        MoleasyCLI(args)
+
+    elif (len(args) > 2) and ('gui' in args):
+        MoleasyGUI()
+
+    else:
+        raise Exception('If you want to use Moleasy via CLI, use more arguments. If you want to use it via GUI (Graphic Interface) use single arg: "gui".')
 
 
 if __name__ == '__main__':
