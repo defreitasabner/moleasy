b2cd20649911fd72746bcee12c50f8b7de6e93b9
Abner Silveira de Freitas <defreitasabner@gmail.com>
2022-09-01 18:39:31 -0300

    Added docstrings to FileManager methods.

--- src/file_manager/FileManager.py
+++ src/file_manager/FileManager.py
@@ -14,15 +14,18 @@ class FileManager(metaclass=ABCMeta):
     def read_file(input_path: str) -> List[Dict[str, str]]:
         """
         read_file()
-        ===
-        Static Method
         ---
-        Open and read the specified file format and return all data readed in a list of dictionaries.
+        Open and read the specified file extension, based on file manager type, and return all data readed in a list of dictionaries.
         """
         ...
 
     @staticmethod
     @abstractmethod
     def write_file(output_path: str, output_data: List[Dict[str, str]]) -> None:
+        """
+        write_file()
+        ---
+        Write all data stored in a new file. The output file has the extension of the file manager type.
+        """
         ...
 
